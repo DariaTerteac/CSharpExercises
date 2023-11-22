49028de656fafb4c0bccf21ec633107bf0d3d202
DariaTerteac <daria.terteac@rxglobal.com>
2023-11-22 14:57:08 +0200

    Added Task #1

---
+++ ConsoleApp1/Task-1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace MatrixMultiplication
+{
+    class MatrixMultiplication
+    {
+        static void Main(string[] args)
+        {
+            int numberOfRows = 2;
+            int numberOfColumns = 2;
+            int[,] matrixA = { { 1, 2 }, { 3, 4 } };
+            int[,] matrixB = { { 5, 6 }, { 7, 8 } };
+            int[,] matrixC = new int[numberOfRows, numberOfColumns];
+
+            // Method to display matrix
+            static void DisplayMatrix(string label, int[,] matrix)
+            {
+                Console.WriteLine(label);
+                foreach (int row in new[] { 0, 1 })
+                {
+                    foreach (int column in new[] { 0, 1 })
+                        Console.Write(matrix[row, column] + " ");
+                    Console.WriteLine();
+                }
+            }
+
+            // Matrix multiplication
+            for (int i = 0; i < numberOfRows; i++)
+            {
+                for (int j = 0; j < numberOfColumns; j++)
+                {
+                    matrixC[i, j] = 0;
+                    foreach (int k in new[] { 0, 1 })
+                        matrixC[i, j] += matrixA[i, k] * matrixB[k, j];
+                }
+            }
+
+            // Display matrices
+            DisplayMatrix("Matrix A:", matrixA);
+            DisplayMatrix("Matrix B:", matrixB);
+            DisplayMatrix("Matrix C:", matrixC);
+
+        }
+    }
+}
